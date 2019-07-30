9f9bf50fc2ac7f3872ac53bcc35ed5975ea61157
Shengfeng Wang <shengfeng@Shengfengs-MacBook-Pro.local>
2019-07-30 13:56:49 -0400

    updated solver

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -1,102 +1,129 @@
+
+import heapq
+import copy
 class EightPuzzle(object):
 
     def __init__(self, board):
         self.board = board
 
-    def copy(self):
-        return copy.deepcopy(self)
+    def copy(self,board):
+        return copy.deepcopy(board)
 
-    def game_over(self):
-        if (self.board[0] == [0, 1, 2]) and (self.board[1] == [3, 4, 5]) and (self.board[2] == [6, 7, 8]):
+    def game_over(self, board):
+        if board==((1,2,3),(4,5,6),(7,8,0)):
             return True
         return False
 
-    def manhattan_dist(num, curr_coord):
+    def manhattan_dist(self, num, curr_coord):
         exp_coord = {
-            0: (0, 0),
-            1: (0, 1),
-            2: (0, 2),
-            3: (1, 0),
-            4: (1, 1),
-            5: (1, 2),
-            6: (2, 0),
-            7: (2, 1),
-            8: (2, 2)
+            0: (2, 2),
+            1: (0, 0),
+            2: (0, 1),
+            3: (0, 2),
+            4: (1, 0),
+            5: (1, 1),
+            6: (1, 2),
+            7: (2, 0),
+            8: (2, 1)
         }
         return abs(curr_coord[0] - exp_coord.get(num)[0]) + abs(curr_coord[1] - exp_coord.get(num)[1])
 
-    def heuristic(self):
+    def heuristic(self, board):
         total_dist = 0
-        for row in range(len(self.board)):
-            for col in range(len(self.board[0])):
-                total_dist += manhattan_dist(self.board[row][col], (row, col))
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                total_dist += self.manhattan_dist(board[row][col], (row, col))
         return total_dist
 
-    def neighbors(curr_coord):
+    def moves(self, board):
+        empty=self.find(board,0)
         ret = []
-        if curr[0] - 1 >= 0:
-            ret.append((curr[0] - 1, curr[1]))
-        if curr[0] + 1 <= 2:
-            ret.append((curr[0] + 1, curr[1]))
-        if curr[1] - 1 >= 0:
-            ret.append((curr[0], curr[1] - 1))
-        if curr[1] + 1 <= 2:
-            ret.append((curr[0], curr[1] + 1))
+        # move down
+        if empty[0] - 1 >= 0:
+            ret.append(((1, 0),board[empty[0]-1][empty[1]]))
+        # move up
+        if empty[0] + 1 <= 2:
+            ret.append(((-1, 0),board[empty[0]+1][empty[1]]))
+        # move right
+        if empty[1] - 1 >= 0:
+            ret.append(((0, 1),board[empty[0]][empty[1]-1]))
+        # move left
+        if empty[1] + 1 <= 2:
+            ret.append(((0, -1),board[empty[0]][empty[1]+1]))
+        # format: (direction of the move, # of the tile moved)
         return ret
 
-    def is_legal_move(self, tile_to_move_coord):
-        for neighbor in neighbors(tile_to_move_coord):
-            if self.board[neighbor[0]][neighbor[1]] == 0:
-                return True
-        return False
 
-    def legal_moves(self):
+    def find(self,board,num):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j]==num:
+                    return i,j
+        return -1,-1
+
+    def perform_move(self, board, move):
+        empty=self.find(board,0)
+        num_moved=move[1]
+        direction=move[0]
         ret = []
-        for row in range(2):
-            for col in range(2):
-                if self.is_legal_move((row, col)):
-                    ret.append((row, col))
-        return ret
+        for row in board:
+            ret.append(list(row))
+        ret[empty[0]][empty[1]]=num_moved
+        ret[empty[0]-direction[0]][empty[1]-direction[1]]=0
+        return self.board_to_tuple(ret)
 
-    def perform_move(self, tile_to_move_coord):
-        if self.is_legal_move(tile_to_move_coord):
-            for neighbor in neighbors(tile_to_move_coord):
-                if self.board[neighbor[0]][neighbor[1]] == 0:
-                    self.board[neighbor[0]][neighbor[1]] = self.board[tile_to_move_coord[0]][tile_to_move_coord[1]]
-                    self.board[tile_to_move_coord[0]][tile_to_move_coord[1]] = 0
-        else:
-            print("illegal move")
 
-    def board_to_tuple(self):
+    def board_to_tuple(self, board):
         ret = []
-        for row in self.board:
+        for row in board:
             ret.append(tuple(row))
         return tuple(ret)
 
-    # consider iterative deepening, depth-limited needed?
-    def find_soln(self, depth):
-        frontier = [self]
-        # board -> (move, last board)
-        # the keys act as a set of visited nodes
-        pred = {self.board_to_tuple() : (None, None)}
-        curr_depth = 0
-        while len(frontier) > 0:
-            curr = frontier.pop(0)
-            if curr.game_over():
-                return backtrack(pred, curr.board_to_tuple())
-            if curr_depth == depth:
-                evaluate_options()
-            for move, new_board in curr.successors():
-                if (new_board.board_to_tuple() not in pred.keys()):
-                    frontier.append(new_board)
-                    pred[new_board.board_to_tuple()] = (move, curr.board_to_tuple())
-        return None
 
-    def backtrack(pred, goal):
-        path = []
-        curr = goal
-        while curr != None:
-            path.insert(0, pred[curr][0])
-            curr = pred[curr][1]
-        path.remove(None)
-        return path
+    def find_soln(self):
+        d = dict() #keep the parent edges, pointing from child to parent
+        s = dict() #nodes already visited, keep the one with least cost
+        q = []
+        heapq.heapify(q)
+        print(self.board)
+        hscore=self.heuristic(self.board)
+        t=(hscore,self.board_to_tuple(self.board),0,None)
+        heapq.heappush(q,t)
+        d[t]=None
+        lastmove = None
+        while len(q)!=0:
+            # format of temp: h_score, board, g_score, move
+            temp = heapq.heappop(q)
+            g=temp[2]
+            if temp[1] in s and s[temp[1]]<=temp[2]:
+                continue
+            elif self.game_over(temp[1]):
+                lastmove=temp
+                break
+            else:
+                s[temp[1]]=temp[2]
+                for move in self.moves(temp[1]):
+                    boardcpy=self.copy(temp[1])
+                    boardcpy=self.perform_move(boardcpy,move)
+                    h=self.heuristic(boardcpy)
+                    t=(g+1+h,boardcpy,g+1,move)
+                    d[t]=temp
+                    heapq.heappush(q,t)
+        if lastmove is None:
+            return None
+        res = []
+        while lastmove is not None:
+            res.append(lastmove[3])
+            lastmove = d.get(lastmove)
+        res.reverse()
+        # format: (direction of the move, # of the tile moved)
+        return res[1:]
+
+
+
+m=[[0,1,3],[4,2,5],[7,8,6]]
+
+p=EightPuzzle(m)
+sol=p.find_soln()
+print(sol)
+print(len(sol))
